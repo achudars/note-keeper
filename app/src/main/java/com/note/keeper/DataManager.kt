792b01a52941c5e2ab6ed68e8a2948367e18618f
Aleks <achudars@gmail.com>
2019-03-03 15:15:06 +0000

    feat: add DataManager

--- app/src/main/java/com/note/keeper/DataManager.kt
+++ app/src/main/java/com/note/keeper/DataManager.kt
@@ -3,4 +3,22 @@ package com.note.keeper
 class DataManager {
     val courses = HashMap<String, CourseInfo>()
     val notes = ArrayList<NoteInfo>()
+
+    init {
+        initialiseCourses()
+    }
+
+    private fun initialiseCourses() {
+        var course = CourseInfo("android_intents", "Android Programming with Intents")
+        courses[course.courseId] = course
+
+        course = CourseInfo("android_async", courseTitle = "Android Async Programming and Services")
+        courses[course.courseId] = course
+
+        course = CourseInfo(courseTitle = "Java Fundamentals: The Java Language", courseId = "java_lang")
+        courses[course.courseId] = course
+
+        course = CourseInfo(courseTitle = "Java Fundamentals: The Core Platform", courseId = "java_core")
+        courses[course.courseId] = course
+    }
 }
