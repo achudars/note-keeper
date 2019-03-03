3d4b81f1f03b6cfd7d5119a89b040a1632449d0e
Aleks <achudars@gmail.com>
2019-03-03 14:58:03 +0000

    feat: add assertJ and refactor the example unit test to use it

--- app/src/test/java/com/note/keeper/ExampleUnitTest.kt
+++ app/src/test/java/com/note/keeper/ExampleUnitTest.kt
@@ -1,16 +1,11 @@
 package com.note.keeper
 
-import org.junit.Assert.assertEquals
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `addition is correct`() {
+        assertThat(4).isEqualTo(2 + 2)
     }
 }
